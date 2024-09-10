9a2f8f3c88f1a623b09828bb9f97f4bc0756b592
renata dias <renata_dias96@live.com>
2024-09-10 10:14:19 -0300

    Criado o readme

---
+++ menu.js
@@ -0,0 +1,67 @@
+// Criação de um menu interativo no terminal para escolher entre as duas funções
+
+const readline = require('readline');
+const {verifyFibonnaci} = require('./Fibonacci.js')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function menu () {
+  console.log('Bem vindo ao menu interativo!');
+  console.log('1. Desafio Fibonacci');
+  console.log('2. Desafio contador de ocorrências da letra "a"');
+  console.log('3. Sair');
+
+  rl.question('Escolha um opção: ', (answer) =>  {
+    handleMenu(answer);
+  })
+  }
+
+function handleMenu (option) {
+  switch(option) {
+    case '1':
+      console.log('---------------------------------------------------------');
+      console.log('Essa função ira verificar se o número que for informado pertence a sequência de Fibonacci');
+      console.log('---------------------------------------------------------');
+      rl.question('Digite um número: ', (numAnswer) => {
+      console.log('---------------------------------------------------------');
+        verifyFibonnaci(Number(numAnswer));
+      console.log('---------------------------------------------------------');
+        console.log('Retornando ao menu!');
+        console.log('---------------------------------------------------------');
+        returnToMenu()
+      });
+      break;
+    case '2':
+      console.log('A função ira retornar a ocorrências da letra "a" na palavra informada')
+      console.log('---------------------------------------------------------');
+      rl.question('Digite uma palvra: ', (string) => {
+        console.log('---------------------------------------------------------');
+        console.log(string);
+        console.log('---------------------------------------------------------');
+        console.log('Retornando ao menu!');
+        console.log('---------------------------------------------------------');
+        returnToMenu()
+      });
+      break;
+    case '3': 
+      console.log('---------------------------------------------------------');
+      console.log('Encerrando!');
+      console.log('---------------------------------------------------------');
+      rl.close();
+      break;
+    default: 
+      console.log('---------------------------------------------------------');
+      console.log('Opção inválida, escolha novamente entre as opções apresentadas!');
+      console.log('---------------------------------------------------------');
+      returnToMenu()
+    }
+}
+
+function returnToMenu() {
+  setTimeout(menu, 1500);
+}
+
+module.exports = {menu};
